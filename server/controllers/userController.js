a1ede6fb42720174f867063ec5454154f30a1677
ArmanM26 <armanmkrtchyan260612@gmail.com>
2025-08-23 14:46:45 +0400

    feat: setup backend and install essential packages

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+import usermodel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+  try {
+  } catch (error) {}
+};
